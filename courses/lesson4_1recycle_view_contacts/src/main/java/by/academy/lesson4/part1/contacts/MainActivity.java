0ca2743ee53a4d94290960c7f0887461d9187a0e
Artem Zolotov <Artem.Zolotov@united-security-providers.ch>
2021-01-22 13:09:49 +0300

    initial code

---
+++ courses/lesson4_1recycle_view_contacts/src/main/java/by/academy/lesson4/part1/contacts/MainActivity.java
@@ -0,0 +1,14 @@
+package by.academy.lesson4.part1.contacts;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
